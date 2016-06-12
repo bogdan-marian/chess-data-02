f1d7a7a9a9025a8a41c6094450f4eb982cd868ee
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-06-12 23:04:15 +0200

    started to work on roundPager

---
+++ app/src/main/java/eu/chessdata/ui/round/RoundPagerFragment.java
@@ -0,0 +1,16 @@
+package eu.chessdata.ui.round;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+import eu.chessdata.utils.Constants;
+
+/**
+ * Created by Bogdan Oloeriu on 6/12/2016.
+ */
+public class RoundPagerFragment extends Fragment {
+    private final String tag = Constants.LOG_TAG;
+
+    private ViewPager mViewPager;
+    private String tournamentKey;
+}
