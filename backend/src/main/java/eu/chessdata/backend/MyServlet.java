4199765e2f2b1f348a608b756cf01800aaac1a33
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-07-02 03:00:44 +0200

    bug related to dependencies

--- backend/src/main/java/eu/chessdata/backend/MyServlet.java
+++ backend/src/main/java/eu/chessdata/backend/MyServlet.java
@@ -6,6 +6,14 @@
 
 package eu.chessdata.backend;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,13 +22,16 @@ import java.util.Scanner;
 
 import javax.servlet.http.*;
 
+import eu.chessdata.backend.model.User;
+import eu.chessdata.backend.utils.Constants;
+
 public class MyServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
 
-        resp.getWriter().println(getFileContent());
+        resp.getWriter().println(getFileContent()+"\n"+listAllAccounts());
     }
 
     @Override
@@ -51,5 +62,39 @@ public class MyServlet extends HttpServlet {
             e.printStackTrace();
         }
         throw new IllegalStateException("Should not be able to reach this point");//return "No content yet:";
+    }
+
+    private String listAllAccounts(){
+        ClassLoader classLoader = getClass().getClassLoader();
+        final StringBuffer sb = new StringBuffer();
+        String filePath = classLoader.getResource("serviceAccountCredentials.json").getFile();
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setServiceAccount(new FileInputStream(filePath))
+                    .setDatabaseUrl(Constants.FIREBASE_URL)
+                    .build();
+            FirebaseApp.initializeApp(options);
+
+            DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference(Constants.USERS);
+            usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot item: dataSnapshot.getChildren()){
+                        User user = item.getValue(User.class);
+                        sb.append(user.getName()+"\n");
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    throw new IllegalStateException(databaseError.getMessage());
+                }
+            });
+
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException(e.getMessage());
+        }
+        sb.append(filePath+"\n");
+        return sb.toString();
     }
 }
---
+++ backend/src/main/java/eu/chessdata/backend/utils/Constants.java
@@ -0,0 +1,10 @@
+package eu.chessdata.backend.utils;
+
+/**
+ * Created by Bogdan Oloeriu on 02/07/2016.
+ */
+public class Constants {
+    public static final String FIREBASE_URL = "https://chess-data.firebaseio.com/";
+    public static final String USERS = "users";
+    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
+}
