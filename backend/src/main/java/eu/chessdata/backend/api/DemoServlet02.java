f43beae63eec5a0b10516bef534995179044f857
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-07-04 00:31:38 +0200

    Example using Gson to decode data

--- backend/src/main/java/eu/chessdata/backend/api/DemoServlet02.java
+++ backend/src/main/java/eu/chessdata/backend/api/DemoServlet02.java
@@ -1,6 +1,11 @@
 package eu.chessdata.backend.api;
 
+import eu.chessdata.backend.model.User;
+import com.google.appengine.repackaged.com.google.gson.Gson;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +19,27 @@ public class DemoServlet02 extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().println("Debug message from: DemoServlet02");
+        resp.getWriter().println("Debug message from: DemoServlet02\n\n");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String json = "";
+        if (br != null){
+            json = json+ br.readLine();
+        }
+        System.out.println("Jons = " + json);
+        resp.getWriter().println("Json: " + json+"\n");
+
+        Gson gson = new Gson();
+        User user = gson.fromJson(json,User.class);
+        String fromObject = "Decoded values: name = " + user.getName()+" email = " + user.getEmail();
+        System.out.println(fromObject);
+        resp.getWriter().println(fromObject);
+
+        User user1 = new User();
+        user1.setName("Bogdan");
+        user1.setEmail("bogdan@mail.com");
+        String json2 = gson.toJson(user1);
+        System.out.println("New json: " + json2);
+
     }
 }
