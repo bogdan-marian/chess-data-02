6ee5e51e213f4a2f5b48a9d6a030ddd5be1b8195
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-07-06 19:44:57 +0200

    decoded payload on server side
    next step: create gae queue

--- backend/src/main/java/eu/chessdata/backend/api/BasicApi.java
+++ backend/src/main/java/eu/chessdata/backend/api/BasicApi.java
@@ -1,5 +1,7 @@
 package eu.chessdata.backend.api;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +10,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import eu.chessdata.backend.model.MyPayLoad;
+import eu.chessdata.backend.utils.MyGson;
 
 /**
  * Created by Bogdan Oloeriu on 05/07/2016.
@@ -22,7 +27,10 @@ public class BasicApi extends HttpServlet {
         if (bufferedReader != null) {
             jsonPlayLoad = jsonPlayLoad + bufferedReader.readLine();
         }
-        resp.getWriter().print(jsonPlayLoad);
-        resp.getWriter().println("OK");
+        Gson gson = MyGson.getGson();
+        MyPayLoad myPayLoad = gson.fromJson(jsonPlayLoad, MyPayLoad.class);
+        if (myPayLoad.getEvent() == MyPayLoad.Event.GAME_RESULT_UPDATED){
+            resp.getWriter().print("Time to notify for game: " + myPayLoad.getGameLocation());
+        }
     }
 }
