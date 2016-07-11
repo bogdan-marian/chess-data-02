2094f8e3e9bbb526c1cb06be35f741b4eba3f422
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-07-11 22:44:28 +0200

    idedified devices on the backend

---
+++ backend/src/main/java/eu/chessdata/backend/model/MySecurityValues.java
@@ -0,0 +1,16 @@
+package eu.chessdata.backend.model;
+
+/**
+ * Created by Bogdan Oloeriu on 7/11/2016.
+ */
+public class MySecurityValues {
+    private String firebaseServerKey;
+
+    public String getFirebaseServerKey() {
+        return firebaseServerKey;
+    }
+
+    public void setFirebaseServerKey(String firebaseServerKey) {
+        this.firebaseServerKey = firebaseServerKey;
+    }
+}
--- backend/src/main/java/eu/chessdata/backend/taskqueue/push/Worker.java
+++ backend/src/main/java/eu/chessdata/backend/taskqueue/push/Worker.java
@@ -10,6 +10,8 @@ import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.logging.Logger;
 
@@ -18,7 +20,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import eu.chessdata.backend.model.Device;
+import eu.chessdata.backend.model.Game;
 import eu.chessdata.backend.model.MyPayLoad;
+import eu.chessdata.backend.model.Player;
 import eu.chessdata.backend.model.User;
 import eu.chessdata.backend.utils.Constants;
 import eu.chessdata.backend.utils.MyFirebase;
@@ -42,7 +47,8 @@ public class Worker extends HttpServlet {
         MyPayLoad myPayLoad = gson.fromJson(jsonPlayLoad, MyPayLoad.class);
 
         if (myPayLoad.getEvent() == MyPayLoad.Event.GAME_RESULT_UPDATED) {
-            notifyUsers(myPayLoad);
+            log.info("Time to call: notifyUsersGameResultUpdated");
+            notifyUsersGameResultUpdated(myPayLoad);
         }
 
         MyFirebase.makeSureEverythingIsInOrder();
@@ -75,8 +81,118 @@ public class Worker extends HttpServlet {
 
     }
 
-    private void notifyUsers(MyPayLoad myPayLoad){
-        //get the users to notify
+    private void notifyUsersGameResultUpdated(MyPayLoad myPayLoad) {
+        if (myPayLoad.getEvent() != MyPayLoad.Event.GAME_RESULT_UPDATED) {
+            return;
+        }
+        String gameLocation = myPayLoad.getGameLocation();
+        if (gameLocation == null) {
+            return;
+        }
+        log.info("notifyUsersGameResultUpdated: debug1");
+        try {
+            MyFirebase.makeSureEverythingIsInOrder();
+            //get the game
+            final Game game = new Game();
+            game.setTableNumber(-1);
+            final CountDownLatch latch01 = new CountDownLatch(1);
+            DatabaseReference gameRef = FirebaseDatabase.getInstance().getReference(gameLocation);
+            gameRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.getValue() != null) {
+                        Game newGame = dataSnapshot.getValue(Game.class);
+                        if (newGame != null) {
+                            game.copyValues(newGame);
+                        }
+                    }
+                    latch01.countDown();
+                }
 
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    log.info("firebase error: " + databaseError.getMessage());
+                    latch01.countDown();
+                }
+            });
+
+            latch01.await();
+            log.info("notifyUsersGameResultUpdated: debug2 end of await: ");
+            if (game.getTableNumber() == -1) {
+                return;
+            }
+            //wee have data
+            if (game.getWhitePlayer() != null) {
+                Player whitePlayer = game.getWhitePlayer();
+                List<Device> devices = getDevicesToNotify(whitePlayer.getPlayerKey());
+                log.info("debug 3: device size = " + devices.size()+"for playerKey = " + whitePlayer.getPlayerKey());
+                for(Device device: devices) {
+                    log.info("I should notify device: " + device.getDeviceType()+":"+ device.getDeviceKey());
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<Device> getDevicesToNotify(String playerKey) {
+        final List<String> userKeys = new ArrayList<>();
+        final List<Device> devices = new ArrayList<>();
+        String globalFollowersLoc = Constants.LOCATION_GLOBAL_FOLLOWERS_BY_PLAYER
+                .replace(Constants.PLAYER_KEY, playerKey);
+        DatabaseReference globalFollowersRef = FirebaseDatabase.getInstance().getReference(globalFollowersLoc);
+        try {
+            final CountDownLatch latch = new CountDownLatch(1);
+            globalFollowersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.hasChildren()) {
+                        for (DataSnapshot item : dataSnapshot.getChildren()) {
+                            User user = item.getValue(User.class);
+                            userKeys.add(user.getUserKey());
+                        }
+                    }
+                    latch.countDown();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    latch.countDown();
+                }
+            });
+
+            latch.await();
+
+            for (String userKey: userKeys){
+                final CountDownLatch secondLatch = new CountDownLatch(1);
+                String myDevicesLoc = Constants.LOCATION_MY_DEVICES
+                        .replace(Constants.USER_KEY,userKey);
+                DatabaseReference myDevicesRef = FirebaseDatabase.getInstance().getReference(myDevicesLoc);
+                myDevicesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChildren()){
+                            for (DataSnapshot item: dataSnapshot.getChildren()){
+                                Device device = item.getValue(Device.class);
+                                if (device != null){
+                                    devices.add(device);
+                                }
+                            }
+                        }
+                        secondLatch.countDown();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        secondLatch.countDown();
+                    }
+                });
+                secondLatch.await();
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return devices;
     }
 }
