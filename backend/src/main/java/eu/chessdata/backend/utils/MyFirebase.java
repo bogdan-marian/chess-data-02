7bc469aa7c65199210bd5f592f95f2c2437c5ea7
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-08-30 00:09:27 +0200

    commented out FirebaseApp.initializeApp(options);

--- backend/src/main/java/eu/chessdata/backend/utils/MyFirebase.java
+++ backend/src/main/java/eu/chessdata/backend/utils/MyFirebase.java
@@ -1,17 +1,11 @@
 package eu.chessdata.backend.utils;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 /**
  * Created by Bogdan Oloeriu on 06/07/2016.
  */
 public class MyFirebase {
-    public static void makeSureEverythingIsInOrder(){
-        if (ourInstance == null){
+    public static void makeSureEverythingIsInOrder() {
+        if (ourInstance == null) {
             throw new IllegalStateException("Firebase is not OK");
         }
     }
@@ -25,14 +19,14 @@ public class MyFirebase {
     private MyFirebase() {
         ClassLoader classLoader = getClass().getClassLoader();
         String accountPath = classLoader.getResource("serviceAccountCredentials.json").getFile();
-        try {
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setServiceAccount(new FileInputStream(accountPath))
-                    .setDatabaseUrl(Constants.FIREBASE_URL)
-                    .build();
-            FirebaseApp.initializeApp(options);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setServiceAccount(new FileInputStream(accountPath))
+//                    .setDatabaseUrl(Constants.FIREBASE_URL)
+//                    .build();
+//            FirebaseApp.initializeApp(options);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
     }
 }
