607108cf38e320aef8abd7f6332e6bce433258b2
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2018-02-10 17:36:19 +0100

    added arguments to StandingsPagerFragment

--- app/src/main/java/eu/chessout/ui/standings/StandingsPagerFragment.java
+++ app/src/main/java/eu/chessout/ui/standings/StandingsPagerFragment.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +14,14 @@ import eu.chessout.utils.Constants;
 
 
 public class StandingsPagerFragment extends Fragment {
+    private final String tag = Constants.LOG_TAG;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View fragmentView = inflater.inflate(R.layout.fragment_standings_pager, container,false);
-        return fragmentView;
-    }
+    private ViewPager mViewPager;
+    private String mTournamentKey;
+    private int mTotalrounds  = 1;
+    private int mRoundsWithData = 1;
+    private String mClubKey;
+    private boolean roundZero = false;
 
     public static Bundle getBundle(String tournamentKey, String clubKey) {
         Bundle bundle = new Bundle();
@@ -28,4 +29,19 @@ public class StandingsPagerFragment extends Fragment {
         bundle.putString(Constants.CLUB_KEY, clubKey);
         return bundle;
     }
+
+    private void setParameters(){
+        mTournamentKey = getArguments().getString(Constants.TOURNAMENT_KEY);
+        mClubKey = getArguments().getString(Constants.CLUB_KEY);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setParameters();
+        View fragmentView = inflater.inflate(R.layout.fragment_standings_pager, container,false);
+        return fragmentView;
+    }
+
+
 }
