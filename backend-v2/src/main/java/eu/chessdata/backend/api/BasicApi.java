903be577ebd43bea0980f6095501e005925c78e2
Bogdan <bogdan.oloeriu@gmail.com>
2019-09-30 19:44:59 +0200

    added backend legacy code but it is still not working

---
+++ backend-v2/src/main/java/eu/chessdata/backend/api/BasicApi.java
@@ -0,0 +1,56 @@
+package eu.chessdata.backend.api;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.gson.Gson;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import eu.chessdata.backend.model.MyPayLoad;
+import eu.chessdata.backend.utils.MyGson;
+
+/**
+ * Created by Bogdan Oloeriu on 05/07/2016.
+ */
+@RestController
+public class BasicApi {
+    private static final Logger log = Logger.getLogger(BasicApi.class.getName());
+
+    //@Override
+    @GetMapping("/api/BasicApi")
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String jsonPlayLoad = "";
+        if (bufferedReader != null) {
+            jsonPlayLoad = jsonPlayLoad + bufferedReader.readLine();
+        }
+        Gson gson = MyGson.getGson();
+        MyPayLoad myPayLoad = gson.fromJson(jsonPlayLoad, MyPayLoad.class);
+        if (myPayLoad.getEvent() == MyPayLoad.Event.GAME_RESULT_UPDATED) {
+            resp.getWriter().print("Time to notify for game: " + myPayLoad.getGameLocation());
+        }
+
+        //add the task to the default queue
+        Queue queue = QueueFactory.getDefaultQueue();
+        queue.add(TaskOptions.Builder.withUrl("/worker").payload(jsonPlayLoad));
+        resp.getWriter().print("Task added to default queue: " + bufferedReader);
+        log.info("bogdanTag Task added to default queue: " + bufferedReader);
+
+//        String firebaseId = MySecurityValues.securityValues.getFirebaseServerKey();
+//        System.out.println("Firebase id = " + firebaseId);
+
+    }
+}
